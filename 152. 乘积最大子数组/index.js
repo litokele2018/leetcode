54c88a34b6a0c9e4b089706034494566736da3dd
litokele <litokele2018@gmail.com>
2020-05-18 13:36:07 +0800

    152.乘积最大子数组

--- 152. 乘积最大子数组/index.js
+++ 152. 乘积最大子数组/index.js
@@ -6,14 +6,15 @@ var maxProduct = (nums) => {
   let res = nums[0];
   let prevMin = nums[0];
   let prevMax = nums[0];
-  let temp1 = 0, temp2 = 0;
+  let temp1 = 0,
+    temp2 = 0;
   let len = nums.length;
   for (let i = 1; i < len; i++) {
-      temp1 = prevMin * nums[i];
-      temp2 = prevMax * nums[i];
-      prevMin = Math.min(temp1, temp2, nums[i]);
-      prevMax = Math.max(temp1, temp2, nums[i]);
-      res = Math.max(prevMax, res);
+    temp1 = prevMin * nums[i];
+    temp2 = prevMax * nums[i];
+    prevMin = Math.min(temp1, temp2, nums[i]);
+    prevMax = Math.max(temp1, temp2, nums[i]);
+    res = Math.max(prevMax, res);
   }
   return res;
 }
